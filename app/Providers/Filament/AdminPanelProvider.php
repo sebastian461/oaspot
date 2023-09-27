20774f6a5824180f5ee7f2d12b794af2c9b387b7
sebastian461 <sebastiano-461@hotmail.com>
2023-09-27 12:10:46 -0500

    Creation of roles and permissions, and assignment

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,42 +17,46 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
+
 
 class AdminPanelProvider extends PanelProvider
 {
-    public function panel(Panel $panel): Panel
-    {
-        return $panel
-            ->default()
-            ->id('admin')
-            ->path('admin')
-            ->login()
-            ->colors([
-                'primary' => Color::Amber,
-            ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
-            ->authMiddleware([
-                Authenticate::class,
-            ]);
-    }
+	public function panel(Panel $panel): Panel
+	{
+		return $panel
+			->default()
+			->id('dashboard')
+			->path('dashboard')
+			->login()
+			->colors([
+				'primary' => Color::Amber,
+			])
+			->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+			->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+			->pages([
+				Pages\Dashboard::class,
+			])
+			->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+			->widgets([
+				Widgets\AccountWidget::class,
+				//Widgets\FilamentInfoWidget::class,
+			])
+			->profile()
+			->middleware([
+				EncryptCookies::class,
+				AddQueuedCookiesToResponse::class,
+				StartSession::class,
+				AuthenticateSession::class,
+				ShareErrorsFromSession::class,
+				VerifyCsrfToken::class,
+				SubstituteBindings::class,
+				DisableBladeIconComponents::class,
+				DispatchServingFilamentEvent::class,
+			])
+			->plugin(FilamentSpatieRolesPermissionsPlugin::make())
+			->authMiddleware([
+				Authenticate::class,
+			]);
+	}
 }
