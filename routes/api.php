1e6c8a1901c068fd730341fa855831e35b820c00
sebastian461 <sebastiano-461@hotmail.com>
2023-10-02 18:21:02 -0500

    ParkingController
    The parking controller was created with parking search and API functions

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ParkingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,4 +27,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function () {
   /* Authenticate */
   Route::post('logout', [AuthController::class, 'logout']);
+
+  /* Parking */
+  Route::get('parking', [ParkingController::class, 'index']);
 });
